58da04add812add28355f33dc82218393860e719
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-07 23:28:45 +0530

    feat:- variable and datatype

---
+++ Chai aur Code/01_variable.js
@@ -0,0 +1,11 @@
+let accountNumber = 1234;
+const nameUser = "Ashish";
+
+var AccountHolderName = "ASHISH KR";
+
+branch = "Noida";
+let managerName;
+
+// console.log(accountNumber, nameUser, AccountHolderName, branch);
+
+console.table([accountNumber, nameUser, AccountHolderName, branch, managerName]);
---
+++ Chai aur Code/02_dataType.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let number = 123;
+
+// console.log(typeof number)
+
+let score = "33ABC";
+// if you want to any data came only 'Number' format the use below like code
+// let valueNumber = Number(score);
+// if you want to any data came only 'String' format the use below like code
+
+let valueNumber = String(score);
+// here print the value
+console.log(typeof valueNumber);
