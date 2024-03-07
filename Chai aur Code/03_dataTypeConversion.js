b4a75f5b65572c90329d864579e95f4485d514d5
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-08 00:47:30 +0530

    feat:- conversion

---
+++ Chai aur Code/03_dataTypeConversion.js
@@ -0,0 +1,18 @@
+let rollNumber = '33';
+
+console.log(rollNumber);
+
+console.log(typeof rollNumber); // number
+console.log(typeof (rollNumber)); // number
+
+// if you want to data will come in 'Number' format then
+
+let newRollNumber = Number (rollNumber) 
+console.log(typeof newRollNumber) // number
+
+// let loggedIn  = 1
+let loggedIn  = ''
+let dataLoggedIn = Boolean (loggedIn)
+console.log(dataLoggedIn)
+
+// 1 => true, 0 => false, "" => false, "ashish" =>true
