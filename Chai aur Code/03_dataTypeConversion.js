1cdac653ce57049a45f4567daa8a78c5cb2411a2
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-08 20:28:38 +0530

    feat:- data type of operations

--- Chai aur Code/03_dataTypeConversion.js
+++ Chai aur Code/03_dataTypeConversion.js
@@ -1,18 +1,29 @@
 let rollNumber = '33';
 
-console.log(rollNumber);
+// console.log(rollNumber);
 
-console.log(typeof rollNumber); // number
-console.log(typeof (rollNumber)); // number
+// console.log(typeof rollNumber); // number
+// console.log(typeof (rollNumber)); // number
 
 // if you want to data will come in 'Number' format then
 
 let newRollNumber = Number (rollNumber) 
-console.log(typeof newRollNumber) // number
+// console.log(typeof newRollNumber) // number
 
 // let loggedIn  = 1
 let loggedIn  = ''
 let dataLoggedIn = Boolean (loggedIn)
-console.log(dataLoggedIn)
+// console.log(dataLoggedIn)
 
 // 1 => true, 0 => false, "" => false, "ashish" =>true
+
+// -----------------------------------------------------------------
+
+// ----------------------------------------------Operations--------------------
+
+console.log(1+1) //2
+console.log(1+"1") //11
+console.log("1"+2) //12
+console.log("1"+"2") // 12
+
+console.log(1+2+'2') //32
