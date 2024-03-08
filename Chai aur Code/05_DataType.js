ba7b0048266babc592677071f88d727145418f49
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-08 22:52:18 +0530

    feat:- array and object declartion

---
+++ Chai aur Code/05_DataType.js
@@ -0,0 +1,25 @@
+// Primitive - (Call by value hota hai)
+//  7-  String, Number, Boolean, BigInt , null {empty}, undefined, Symbol -{kisi value ko unique banane ke liye use}
+
+// Non-primitive ( reference type or Call by reference)
+
+//memory allocation in references
+
+// Array, Object
+
+// Example of Symbol
+
+let id = Symbol ('123')
+let anotherId = Symbol ('123')
+
+// console.log(id === anotherId) // false
+// -----------------------------------------------------
+
+let heroName = ['Mahesh', 'RajaniKant', 'Surya'] // array declarations
+console.log(heroName)
+let moviesName = {
+    moviesYear: 2013,
+    Hero: 'SanjayDat'
+}
+
+console.log(moviesName)
