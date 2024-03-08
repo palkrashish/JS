91041ae28d15025fb99e0869c85f9bfbd7e73679
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-08 22:18:41 +0530

    feat:- comparision

--- Chai aur Code/03_dataTypeConversion.js
+++ Chai aur Code/03_dataTypeConversion.js
@@ -21,13 +21,33 @@ let dataLoggedIn = Boolean (loggedIn)
 
 // ----------------------------------------------Operations--------------------
 
-console.log(1+1) //2
-console.log(1+"1") //11
-console.log("1"+2) //12
-console.log("1"+"2") // 12
+// console.log(1+1) //2
+// console.log(1+"1") //11
+// console.log("1"+2) //12
+// console.log("1"+"2") // 12
 
-console.log(1+2+'2') //32
-console.log('1'+2+2)// 122
+// console.log(1+2+'2') //32
+// console.log('1'+2+2)// 122
 
-console.log(true) //true
-console.log(+true) //1
+// console.log(true) //true
+// console.log(+true) //1
+
+// -data type  comparision---------------
+
+// console.log('2' > 0) // true
+// console.log('02' >= 0) // true
+
+// console.log(2 <= 0)
+// console.log(2 <= '0')
+
+// console.log(undefined > 0) //false
+// console.log(undefined == 0) //false
+// console.log(undefined >= 0) //false
+// console.log(undefined <= 0) //false
+
+// console.log(null >0)  //false
+// console.log(null >=0) // true
+// console.log(null ==0) //false
+// console.log(null <=0) //true
+
+console.log('2'===2) //false
