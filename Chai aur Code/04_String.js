f6b5ab16ab1807ccc13af5b3bd10741926d27bb2
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-09 13:05:54 +0530

    feat:- string interpolation /backticks

---
+++ Chai aur Code/04_String.js
@@ -0,0 +1,12 @@
+let userName = 'Ashish'
+console.log(userName)
+
+console.log(`My name is '${userName}' and lives in Delhi `)
+
+// another way string declare
+
+let anotherWay = new String('Ashish')
+// console.log(anotherWay.__proto__)
+
+console.log(userName.length) //6
+console.log(userName.toUpperCase()) // ASHISH
