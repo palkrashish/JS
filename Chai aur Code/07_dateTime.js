f73838f7c694180687ccddc4d0f6dc6ebcedc66b
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-09 18:53:26 +0530

    feat :- Date and Time

---
+++ Chai aur Code/07_dateTime.js
@@ -0,0 +1,37 @@
+// let Date = 2024
+
+// console.log(Date)
+// console.log(typeof Date)
+
+
+// let myDate = new Date ()
+// console.log(myDate) //2024-03-09T12:52:21.053Z
+// console.log(myDate.toString()) //Sat Mar 09 2024 18:22:21 GMT+0530 (India Standard Time)
+// console.log(myDate.toLocaleDateString()) /3/9/2024
+
+// console.log(myDate.toDateString()) //Sat Mar 09 2024
+// console.log(myDate.toISOString()) //2024-03-09T12:54:33.261Z
+
+// console.log(myDate.toLocaleString()) //3/9/2024, 6:24:55 PM
+// console.log(myDate.toLocaleTimeString()) //3/9/2024, 6:24:55 PM
+
+// console.log(myDate.toTimeString()) //18:25:45 GMT+0530 (India Standard Time
+// console.log(myDate.toUTCString()) //Sat, 09 Mar 2024 12:56:12 GMT
+
+// --------------------------Date.now()------------------------
+// let myDate = new Date (2024, 0, 19)
+
+// console.log(myDate)
+// console.log(myDate.toLocaleString()) //1/9/2024, 12:00:00 AM
+// console.log(myDate.toDateString()) 
+//Tue Jan 09 2024
+
+// ----- date--------------
+
+let againDate = new Date()
+
+console.log(againDate.toLocaleDateString()) //3/9/2024
+ 
+console.log(againDate.toDateString()) //Sat Mar 09 2024
+
+
