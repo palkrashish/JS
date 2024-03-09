4049aac9077ed3221792df37c06858dc99e63269
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-09 17:08:52 +0530

    feat:- string learn

--- Chai aur Code/04_String.js
+++ Chai aur Code/04_String.js
@@ -1,4 +1,4 @@
-let userName = 'Ashish'
+let userName = 'Ashish-Kr-Pal'
 console.log(userName)
 
 console.log(`My name is '${userName}' and lives in Delhi `)
@@ -10,3 +10,24 @@ let anotherWay = new String('Ashish')
 
 console.log(userName.length) //6
 console.log(userName.toUpperCase()) // ASHISH
+console.log(userName) //ashish
+
+// charAt() 
+console.log(userName.charAt(0))
+//lastIndexOf()
+console.log(userName.lastIndexOf('h'))
+
+let name = '      Ashish           '
+
+console.log(name)
+console.log(name.trim())
+
+console.log(userName.includes('sh')) //true
+
+console.log(userName.slice(0,3)) //Ash
+let array = [1,2,3,4,5,6]
+
+console.log(array.splice(0,2)) //[1,2]
+
+console.log(userName.split('-')) //[ 'Ashish', 'Kr', 'Pal' ]
+
