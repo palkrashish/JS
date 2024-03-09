c017cb46eb05a9f7ae743b4a41d4697dde9a29ea
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-09 18:13:48 +0530

    feat :- Numbr and Math

---
+++ Chai aur Code/06_numbersandmath.js
@@ -0,0 +1,42 @@
+const score = 1040.044;
+// console.log(score);
+// console.log(typeof score);
+
+let balance = new Number(98);
+
+// console.log(balance);
+
+// console.log(typeof balance);
+
+// console.log(score) //1040.044
+// console.log(score.toFixed()) //1040
+
+let numberValue = 101001010
+
+// console.log(numberValue.toLocaleString('en-In'))
+
+let accountBalance = 100
+// console.log(accountBalance.toFixed(2))
+
+
+
+
+// --------------------------------Math---------------------
+// console.log(Math) //Object [Math] {}
+
+// console.log(Math.abs(-3210)) //3210
+// console.log(Math.round(4.2939)) //4
+// console.log(Math.ceil(4.2939)) //5
+// console.log(Math.floor(4.2939)) // 4
+// console.log(Math.min(3,2,49,3030,12,3)) // 2
+// console.log(Math.max(3,2,49,3030,12,3)) // 3030
+
+console.log(Math.random()) //0.4911511869569576
+console.log((Math.random() *5)+1) //4.686979026444437
+
+console.log(Math.floor(Math.random() * 10 + 1)); //  4 or 5 or 6 or 2 or 9
+
+const min = 5;
+const max = 10;
+
+console.log(Math.floor(Math.random() * (max-min +1))+min) // 5 or 5 or 6 or 8 or 7
