2abe7a0aba5f37d34913eaad0aa1dd8f6d452af7
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-11 12:30:26 +0530

    feat :- Object declaration and use

--- Chai aur Code/09_Object.js
+++ Chai aur Code/09_Object.js
@@ -1,23 +1,31 @@
 // Symbol declare
 
-const mySym  =  Symbol ('key1')
+const mySym = Symbol("key1");
 
 const User = {
-    name: 'Ashish',
-    'full name': 'Ashish Kr Pal',
-    age: 19,
-    [mySym] : 'mykey1',
-    college: 'IMS',
-    email: 'example@gmail.com',
-    islogged: 'yes',
-    // details: ('Monday', 'Tuesday','Wednesday','Thursday') // it will print Thursday
-    details: ['Monday', 'Tuesday','Wednesday','Thursday']
-}
+  name: "Ashish",
+  "full name": "Ashish Kr Pal",
+  age: 19,
+  [mySym]: "mykey1",
+  college: "IMS",
+  email: "example@gmail.com",
+  islogged: "yes",
+  // details: ('Monday', 'Tuesday','Wednesday','Thursday') // it will print Thursday
+  details: ["Monday", "Tuesday", "Wednesday", "Thursday"],
+};
 
-console.log(User)
-console.log(User.email) // example@gmail.com
-console.log(User.details[0]) // Monday
+// console.log(User)
+// console.log(User.email) // example@gmail.com
+// console.log(User.details[0]) // Monday
 
-console.log(User["full name"]) // Ashish Kr Pal
+// console.log(User["full name"]) // Ashish Kr Pal
 
-console.log(User[mySym])
+// console.log(User[mySym])
+
+User.email = "js@gmail.com";
+// console.log(User);
+
+User.geet = function () {
+  console.log(`Welcome to the page  '${this.name}' `);
+};
+console.log(User.geet())
