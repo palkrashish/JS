782dc64d8b0796733fd9cdc3575fbe7db1918f21
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-11 11:18:59 +0530

    feat:- Object declare and Symbol

---
+++ Chai aur Code/09_Object.js
@@ -0,0 +1,23 @@
+// Symbol declare
+
+const mySym  =  Symbol ('key1')
+
+const User = {
+    name: 'Ashish',
+    'full name': 'Ashish Kr Pal',
+    age: 19,
+    [mySym] : 'mykey1',
+    college: 'IMS',
+    email: 'example@gmail.com',
+    islogged: 'yes',
+    // details: ('Monday', 'Tuesday','Wednesday','Thursday') // it will print Thursday
+    details: ['Monday', 'Tuesday','Wednesday','Thursday']
+}
+
+console.log(User)
+console.log(User.email) // example@gmail.com
+console.log(User.details[0]) // Monday
+
+console.log(User["full name"]) // Ashish Kr Pal
+
+console.log(User[mySym])
