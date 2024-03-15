ea2c090d40ca6b535cae5cc5472352f0e8071c10
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-15 11:30:50 +0530

    feat:- function declaration array, object and use them

--- Chai aur Code/10_Function.js
+++ Chai aur Code/10_Function.js
@@ -10,10 +10,52 @@ function addition(num, num1) {
 
 function sayHello(user = "Guest") {
   if (!user) {
-    console.log("Kindly inpur user name");
+    // console.log("Kindly inpur user name");
     return;
   }
   return ` ${user} loging successfully. `;
 }
 // console.log(sayHello('Ashish'));
-console.log(sayHello());
+// console.log(sayHello());
+
+// ----------------------------------2nd Video function-------------------
+
+function cardItemAdditon(val1, ...val) {
+  return val;
+}
+
+// console.log(cardItemAdditon(1,12,13,14,15,16,17))
+
+const detailsofHotel = {
+  hotelPrice: 1933,
+  hotelName: "Taj",
+};
+
+function hotelBooking(anyObj) {
+  console.log(
+    // `Welcome to booking hotel ${anyObj.hotelName} and the price ${anyObj.hotelPrice} `
+  );
+}
+
+// hotelBooking(detailsofHotel);
+
+// we can do like this as well
+hotelBooking({
+  hotelName: 'R K hotel',
+  hotelPrice: 2999
+
+})
+// function with Array 
+
+const arrayNumber = [11,12,13,14]
+
+function newArrayNumber (getArrray){
+
+  return getArrray
+}
+
+// console.log(newArrayNumber(arrayNumber))
+
+console.log(newArrayNumber([15,16,17,18]))
+
+
