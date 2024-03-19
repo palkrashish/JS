b7799d1f83e3186f73835bbbfb4cd701e3115bd9
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-19 14:48:53 +0530

    feat:- loop learn

---
+++ Chai aur Code/14_Loop.js
@@ -0,0 +1,13 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//For loop
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  // console.log(element)
+}
+
+
+// forEach loop
+array.forEach((element) => {
+  console.log(element);
+});
+
