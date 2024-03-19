d07aba6f4321904fbfeea2228fad744d023f6b65
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-03-19 11:13:18 +0530

    feat- control fllow conditional operator

---
+++ Chai aur Code/13_ControlFlow.js
@@ -0,0 +1,14 @@
+const number = 100
+
+// if (1000 > number ){
+//     console.log('1000 is greater than', number)
+// }
+// else{
+//     console.log('1000 is less than', number)
+// }
+// ternary operator
+
+// condition ? true : false
+let value = 100
+
+value <=120 ? console.log('120 is greate than', value): console.log('120 is less than', value)
